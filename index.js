660cf695ae46c454e7bc6aae637f4faa35b9e998
Joakim Bengtson <joakim.bengtson@gmail.com>
2014-10-08 14:57:19 +0200

    Adding values/services methods

--- index.js
+++ index.js
@@ -4,20 +4,24 @@ var Promise = require('bluebird');
 module.exports = exports = function init (options) {
   var api = {
     service: getService.bind(null, options.host),
-    value: getValue.bind(null, options.host)
+    services: getService.bind(null, options.host, ''),
+    value: getValue.bind(null, options.host),
+    values: getValue.bind(null, options.host, '')
   };
 
   if (options.promise) {
     return {
       service: Promise.promisify(api.service),
+      services: Promise.promisify(api.services),
       value: Promise.promisify(api.value)
+      values: Promise.promisify(api.values)
     }
   }
 
   return api;
 };
 
-function getService (host, service, cb) {
+function getService (host, service, opts, cb) {
   request(host + '/service/' + service, {json: true}, function (err, res, services) {
     if (err) {
       return cb(err);
